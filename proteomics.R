19058b479865465f21119e8bb939ecec016f7926
Paulien L <paulien.leten@gmail.com>
2018-10-16 12:17:08 +0200

    Renamed sheets to exclude spaces

--- proteomics.R
+++ proteomics.R
@@ -1,9 +1,21 @@
 library(tidyverse)
 library(openxlsx)
 
+# Reading in excel workbook
 proteomics_data <- loadWorkbook('results_yeast_analysis.xlsx')
+
+# Renaming sheets to removes spaces in names
 sheetNames <- sheets(proteomics_data)
+newSheetNames = c()
+for (sheetName in sheetNames) {
+  sheetName = gsub(" ", "_", sheetName)
+  newSheetNames[sheetName] = sheetName 
+}
+sheetNames = newSheetNames
+
+# Assigning each sheet to a dataframe
 for(i in 1:length(sheetNames))
 {
   assign(sheetNames[i],readWorkbook(proteomics_data,sheet = i))
 }
+
