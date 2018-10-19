45124e9a571c32a27d0bc783e5ef9a4382069b20
Paulien L <paulien.leten@gmail.com>
2018-10-19 18:12:50 +0200

    Adjusted comments

--- mutation_clones.R
+++ mutation_clones.R
@@ -3,10 +3,10 @@ library(openxlsx)
 # Reading in excel workbook
 mutation_clones_data = loadWorkbook('snps_indels_clones.xlsx') # Replace this with the apporpiate file
 
-# Renaming sheets to removes spaces in names
+# Getting sheet names
 sheetNames = sheets(mutation_clones_data)
 
-# Assigning each sheet to a dataframe
+# Assigning each sheet to a dataframe (starts at 2nd row to lose ploidy header)
 for(i in 1:length(sheetNames)){
   assign(sheetNames[i],readWorkbook(mutation_clones_data,sheet = i, startRow = 2))
 }
