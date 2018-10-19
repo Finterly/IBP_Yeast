b54bbc6f6111d52dad58d0760867a78da0b46c0c
Paulien L <paulien.leten@gmail.com>
2018-10-19 17:37:32 +0200

    Changed significant parameter to boolean. Added dataframe list

--- proteomics.R
+++ proteomics.R
@@ -21,4 +21,11 @@ for(i in 1:length(sheetNames))
   assign(sheetNames[i],readWorkbook(proteomics_data,sheet = i))
 }
 
+data_frame_list = list(S1 = S1_all_6perc_vs_all_YPD, S2 = S2_ANC_6pers_vs_YPD, S3 = S3_77_6pers_vs_YPD, 
+                    S4 = S4_78_6pers_vs_YPD, S5 = S5_79_6perc_vs_YPD, S6 = S6_86_6perc_vs_YPD,
+                    S7 = S7_87_6perc_vs_YPD, S8_88_6perc_vs_YPD)
 
+data_frame_list = lapply(data_frame_list, function(df) {
+  df$Significant = as.factor(!(is.na(df$Significant)))
+  df
+})
